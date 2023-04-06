775bffc8a52ced4664bf72e4387ae37839183098
Zulfikar Fahmi falakh <zulfikar@fivejack.com>
2023-04-06 10:18:55 +0700

    New: add too many redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/favorite",
+        permanent: true,
+      },
+      {
+        source: "/favorite",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
